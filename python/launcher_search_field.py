115adf88289620626593f2e75398d1bdb2937fb9
Lorien <2249297094@qq.com>
2026-06-26 14:35:13 +0800

    style: give search field layered liquid glass

--- python/launcher_search_field.py
+++ python/launcher_search_field.py
@@ -57,19 +57,37 @@ class SearchField(tk.Canvas):
             highlightthickness=0,
             bd=0,
         )
-        self._shape = self.create_polygon(
-            *stable_rounded_rectangle_points(
-                1,
-                1,
-                100,
-                self.s(METRICS.search_height) - 1,
-                self.s(METRICS.input_radius),
-            ),
+        self._shadow = self.create_polygon(
+            *stable_rounded_rectangle_points(2, 3, 100, self.s(METRICS.search_height) - 1, self.s(METRICS.input_radius)),
             smooth=True,
             splinesteps=24,
-            fill=theme["surface_1"],
-            outline=theme["border"],
+            fill=theme["glass_shadow"],
+            outline="",
+        )
+        self._shape = self.create_polygon(
+            *stable_rounded_rectangle_points(1, 1, 100, self.s(METRICS.search_height) - 3, self.s(METRICS.input_radius)),
+            smooth=True,
+            splinesteps=24,
+            fill=theme["glass_fill"],
+            outline=theme["glass_border"],
             width=self.s(1),
+        )
+        self._inner_rim = self.create_polygon(
+            *stable_rounded_rectangle_points(4, 4, 96, self.s(METRICS.search_height) - 6, max(1, self.s(METRICS.input_radius - 4))),
+            smooth=True,
+            splinesteps=24,
+            fill="",
+            outline=theme["glass_highlight"],
+            width=self.s(1),
+        )
+        self._top_sheen = self.create_line(
+            self.s(18),
+            self.s(4),
+            self.s(70),
+            self.s(4),
+            fill=theme["glass_highlight"],
+            width=self.s(1),
+            capstyle=tk.ROUND,
         )
         self._search_icon = self.create_text(
             self.s(self.metrics.icon_x),
@@ -84,7 +102,7 @@ class SearchField(tk.Canvas):
             relief="flat",
             bd=0,
             highlightthickness=0,
-            bg=theme["surface_1"],
+            bg=theme["glass_content"],
             fg=theme["text_primary"],
             insertbackground=theme["purple_light"],
             selectbackground=theme["surface_selected"],
@@ -131,17 +149,27 @@ class SearchField(tk.Canvas):
             return self.theme["window_bg"]
 
     def _on_configure(self, event):
-        width = max(2, int(event.width))
-        height = max(2, int(event.height))
+        width = max(4, int(event.width))
+        height = max(8, int(event.height))
+        radius = min(self.s(METRICS.input_radius), height // 2 - 1)
+        self.coords(
+            self._shadow,
+            *stable_rounded_rectangle_points(2, 3, width - 2, height - 1, radius),
+        )
         self.coords(
             self._shape,
-            *stable_rounded_rectangle_points(
-                1,
-                1,
-                width - 1,
-                height - 1,
-                self.s(METRICS.input_radius),
-            ),
+            *stable_rounded_rectangle_points(1, 1, width - 1, height - 3, radius),
+        )
+        self.coords(
+            self._inner_rim,
+            *stable_rounded_rectangle_points(4, 4, width - 4, height - 6, max(1, radius - self.s(4))),
+        )
+        self.coords(
+            self._top_sheen,
+            self.s(18),
+            self.s(4),
+            max(self.s(18), width - self.s(30)),
+            self.s(4),
         )
         entry_x = self.s(self.metrics.text_x)
         clear_margin = self.s(self.metrics.clear_x_margin)
@@ -180,9 +208,16 @@ class SearchField(tk.Canvas):
         border = (
             self.theme["border_focus"]
             if self._focused
-            else self.theme["border_hover"] if self._hovered else self.theme["border"]
+            else self.theme["border_hover"]
+            if self._hovered
+            else self.theme["glass_border"]
         )
-        self.itemconfigure(self._shape, outline=border)
+        fill = (
+            self.theme["glass_fill_hover"]
+            if self._focused or self._hovered
+            else self.theme["glass_fill"]
+        )
+        self.itemconfigure(self._shape, outline=border, fill=fill)
         self.itemconfigure(
             self._search_icon,
             fill=self.theme["purple_light"] if self._focused else self.theme["text_secondary"],
